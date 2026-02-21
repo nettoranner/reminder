b2bbeceadba42f3c082140e9fac64dacf739bbda
nettoranner <mattioffwork@gmail.com>
2026-02-21 23:26:36 +0700

    feat: add UserBase model

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,8 +1,25 @@
 from sqlmodel import Field, select, SQLModel
 
 
-class User(SQLModel, table=True):
+class UserBase(SQLModel):
+    name: str = Field(index=True)
+    email: str = Field(index=True)
+
+
+class User(UserBase, table=True):
     """Here will be ORM models for database tables"""
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    email: str 
+    username: str
+
+class UserPublic(UserBase):
+    id: int
+
+
+class UserCreate(UserBase):
+    username: str = Field(..., min_length=5, max_length=20)
+
+
+class UserUpdate(UserBase):
+    name: str | None = None
+    email: str | None = None
+    username: str | None = Field(None, min_length=5, max_length=20)
